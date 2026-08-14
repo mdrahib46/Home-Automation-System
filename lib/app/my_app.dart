aea254184bf9ca4c3482090c96338c3be0810584
mdrahib46 <nahid179846@bsdi-bd.org>
2026-08-14 23:47:47 +0600

    update application code

--- lib/app/my_app.dart
+++ lib/app/my_app.dart
@@ -1,5 +1,5 @@
 import 'package:flutter/material.dart';
-import 'package:smart_home/feature/screen/home_screen.dart';
+import 'package:smart_home/feature/screen/auth/sign_in_screen.dart';
 
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
@@ -8,7 +8,7 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return const MaterialApp(
       debugShowCheckedModeBanner: false,
-      home: HomeScreen(),
+      home: SignInScreen(),
     );
   }
 }
